c7ce5717aed9c966e605bccdf589547e29a0a111
crowne <crow.neil@gmail.com>
2024-03-30 12:15:54 +0000

    Fixed description handling

--- src/lib/editors/FieldWrapper.svelte
+++ src/lib/editors/FieldWrapper.svelte
@@ -2,29 +2,38 @@
 	import { schemaLabel } from "../types/schema";
 	import type { CommonComponentParameters } from "../types/CommonComponentParameters";
 	import { stringToHtml } from "../utilities.js";
+	import Info from '../img/Info.svelte'
 
 	export let params: CommonComponentParameters;
 	export let schema: any;
 
 	const title = schemaLabel(schema, params.path);
-	const id = params.path.join('.');
-	$: error = params.validationErrors[params.path.join('.')];
+	const id = params.path.join(".");
+	$: error = params.validationErrors[params.path.join(".")];
 </script>
 
 {#if params.containerParent !== "array"}
-	<div>
-		<label id={`label-${id}`} for={id}
-			class:required={params.required} class:readonly={schema.readOnly || params.containerReadOnly}>
-			{@html stringToHtml(title)}
-			{#if schema.description}
-				<span class="info" title={schema.description}></span>
-			{/if}
-		</label>	
+	<div class="inline-flex flex-nowrap">
+		<div>
+			<label
+				id={`label-${id}`}
+				for={id}
+				class:required={params.required}
+				class:readonly={schema.readOnly || params.containerReadOnly}
+			>
+				{@html stringToHtml(title)}
+			</label>
+		</div>
+		{#if schema.description}
+			<div title={schema.description} class="size-2.5">
+				<Info size={12} />
+			</div>
+		{/if}
 	</div>
 {/if}
-	<div>
-		<slot></slot>
-		{#if error && params.showErrors}
-			<div class="error">{error}</div>
-		{/if}		
-	</div>
+<div>
+	<slot />
+	{#if error && params.showErrors}
+		<div class="text-red-600">{error}</div>
+	{/if}
+</div>
