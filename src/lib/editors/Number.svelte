91e37a42a98cc453e7a7521feac3d228ca105b63
crowne <crow.neil@gmail.com>
2024-03-31 12:38:26 +0100

    Show red error border on Number

--- src/lib/editors/Number.svelte
+++ src/lib/editors/Number.svelte
@@ -3,6 +3,9 @@
 	export let params: CommonComponentParameters;
 	export let schema: any;
 	export let value: any;
+
+	$: border = (params.showErrors && params.path.join(".") in params.validationErrors) ? "border border-red-600" : "border border-grey-200";
+
 </script>
 
 <!-- event which calls pathChanged should be after all bindings so 'value' will have been updated -->
@@ -14,6 +17,6 @@
 			let val = parseFloat(ev.currentTarget.value);
 			params.pathChanged(params.path, isNaN(val) ? undefined : val);
 		}}
-		class="border w-full"
+		class="{border} w-full"
 	/>
 </svelte:component>
